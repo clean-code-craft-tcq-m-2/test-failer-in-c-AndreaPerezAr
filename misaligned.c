82ed1cb5e3f940d7f4cc4e406949671dc0fb896e
Andrea Pérez <48370962+AndreaPerezAr@users.noreply.github.com>
2022-11-16 09:23:59 -0600

    Update misaligned.c

--- misaligned.c
+++ misaligned.c
@@ -9,7 +9,7 @@ const int numOfMinColors = sizeof(minorColor) / sizeof(minorColor[0]);
 const int totColors = numOfMajColors * numOfMinColors;
 
 void evaluationColorMap(int pairIndex) {
-    static int correctPairNumIdx = 1;
+    static int correctPairNumIdx = 0;
     assert(pairIndex == correctPairNumIdx);
     assert(pairIndex <= totColors);
     assert(pairIndex != 0);
@@ -24,7 +24,7 @@ int printColorMap() {
     int i = 0, j = 0, pairIndex=0;
     for(i = 0; i < 5; i++) {
         for(j = 0; j < 5; j++) {
-            pairIndex = (i * 5) + (j+1);
+            pairIndex = (i * 5 + j);
             printf("%d | %s | %s\n", pairIndex, majorColor[i], minorColor[i]);
             evaluationColorMap(pairIndex);
         }
@@ -34,7 +34,7 @@ int printColorMap() {
 
 int main() {
     int result = printColorMap();
-    assert(result == totColors);
+    assert(result == i * j);
     printf("All is well (maybe!)\n");
     return 0;
 }
